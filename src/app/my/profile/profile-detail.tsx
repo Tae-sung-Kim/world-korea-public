fca7355ed1cd9f4c9842a194dcc5dedca99792a1
김태성 <qwaszx82@danbiedu.co.kr>
2024-07-23 14:43:58 +0900

    간격 조정

--- src/app/my/profile/profile-detail.tsx
+++ src/app/my/profile/profile-detail.tsx
@@ -6,19 +6,11 @@ import { Label } from '@/components/ui/label';
 import userService from '@/services/user.service';
 import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import { toast } from 'sonner';
 
 export default function ProfileDetail() {
   const router = useRouter();
 
-  const {
-    isFetching,
-    isSuccess,
-    isError,
-    data: currentUserData,
-    refetch,
-  } = useQuery({
+  const { isFetching, data: currentUserData } = useQuery({
     queryKey: ['userService.getCurrentUser'],
     queryFn: userService.getCurrentUser,
   });
@@ -27,38 +19,53 @@ export default function ProfileDetail() {
     router.push('/my/profile/edit');
   };
 
-  // useEffect(() => {
-  //   if (isSuccess) {
-  //     refetch();
-  //     // toast.success('정보 조회가 완료 되었습니다.');
-  //   }
-  //   // if (isError) {
-  //   //   toast.error('정보 조회중 오류가 발생하였습니다.');
-  //   // }
-  // }, [isSuccess, isError, refetch]);
-
   return (
     <>
       <h1 className="text-2xl font-semibold text-center p-4">회원 정보</h1>
 
-      <div className="container">
-        <Label>아이디</Label>
-        <Input readOnly disabled value={currentUserData?.loginId ?? ''} />
-        <Label>업체명</Label>
-        <Input readOnly disabled value={currentUserData?.companyName ?? ''} />
-        <Label>업체 번호</Label>
-        <Input readOnly disabled value={currentUserData?.companyNo ?? ''} />
-        <Label>주소</Label>
-        <Input readOnly disabled value={currentUserData?.address ?? ''} />
-        <Label>연락처</Label>
-        <Input readOnly disabled value={currentUserData?.contactNumber ?? ''} />
-        <Label>이름</Label>
-        <Input readOnly disabled value={currentUserData?.name ?? ''} />
-        <Label>휴대폰</Label>
-        <Input readOnly disabled value={currentUserData?.contactNumber ?? ''} />
-        <Label>이메일</Label>
-        <Input readOnly disabled value={currentUserData?.email ?? ''} />
+      <div className="container space-y-8">
+        <div className="space-y-2">
+          <Label>아이디</Label>
+          <Input readOnly disabled value={currentUserData?.loginId ?? ''} />
+        </div>
 
+        <div className="space-y-2">
+          <Label>업체명</Label>
+          <Input readOnly disabled value={currentUserData?.companyName ?? ''} />
+        </div>
+
+        <div className="space-y-2">
+          <Label>업체 번호</Label>
+          <Input readOnly disabled value={currentUserData?.companyNo ?? ''} />
+        </div>
+        <div className="space-y-2">
+          <Label>주소</Label>
+          <Input readOnly disabled value={currentUserData?.address ?? ''} />
+        </div>
+        <div className="space-y-2">
+          <Label>연락처</Label>
+          <Input
+            readOnly
+            disabled
+            value={currentUserData?.contactNumber ?? ''}
+          />
+        </div>
+        <div className="space-y-2">
+          <Label>이름</Label>
+          <Input readOnly disabled value={currentUserData?.name ?? ''} />
+        </div>
+        <div className="space-y-2">
+          <Label>휴대폰</Label>
+          <Input
+            readOnly
+            disabled
+            value={currentUserData?.contactNumber ?? ''}
+          />
+        </div>
+        <div className="space-y-2">
+          <Label>이메일</Label>
+          <Input readOnly disabled value={currentUserData?.email ?? ''} />
+        </div>
         <Button className="ml-2" disabled={isFetching} onClick={handleUpdate}>
           수정하기
         </Button>
