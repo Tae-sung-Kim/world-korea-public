1b55c3f0a8c5e0fe3102aad1346d82a477fd0093
김태성 <qwaszx82@danbiedu.co.kr>
2024-08-27 16:17:02 +0900

    관리자 로딩 개발

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,6 @@
+import Loading from './loading.component';
 import AdminProtectedRoute from '@/app/admin/admin-protected-route.component';
 import { Separator } from '@/components/ui/separator';
-import AdminLoadingProvider from '@/contexts/admin-loading.context';
 import Link from 'next/link';
 import { TbNumber123, TbTicket, TbUser } from 'react-icons/tb';
 
@@ -10,71 +10,70 @@ export default async function AdminLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <AdminLoadingProvider>
-      <AdminProtectedRoute>
-        <div className="container flex py-12 gap-6">
-          <div className="w-[260px] border border-gray-200 bg-white">
-            <div className="flex items-center justify-center py-4 text-white bg-black">
-              <h2 className="text-2xl">관리자 페이지</h2>
-            </div>
-            <ul>
-              <li className="p-5 my-4">
-                <span className="flex items-center gap-1 text-xl font-bold">
-                  상품 관리
-                  <TbTicket />
-                </span>
-                <ul className="mt-2">
-                  <li className="text-sm mt-[2px] text-gray-600">
-                    <Link href="/admin/products"> 상품 목록</Link>
-                  </li>
-                  <li className="text-sm mt-[2px] text-gray-600">
-                    <Link href="/admin/products/create">상품 등록</Link>
-                  </li>
-                </ul>
-              </li>
-              <Separator />
-              <li className="p-5 my-4">
-                <span className="flex items-center gap-1 text-xl font-bold">
-                  회원 관리
-                  <TbUser />
-                </span>
-                <ul className="mt-2">
-                  <li className="text-sm mt-[2px] text-gray-600">
-                    <Link href="/admin/user-categories">회원 구분</Link>
-                  </li>
-                  <li className="text-sm mt-[2px] text-gray-600">
-                    <Link href="/admin/users">회원 목록</Link>
-                  </li>
-                  <li className="text-sm mt-[2px] text-gray-600">
-                    파트너사 목록
-                  </li>
-                </ul>
-              </li>
-              <Separator />
-              <li className="p-5 my-4">
-                <span className="flex text-lg font-bold">
-                  핀번호 관리
-                  <TbNumber123 />
-                </span>
-                <ul className="mt-2">
-                  <li className="text-sm mt-[2px] text-gray-600">
-                    <Link href="/admin/pins">핀번호 목록</Link>
-                  </li>
-                  <li className="text-sm mt-[2px] text-gray-600">
-                    <Link href="/admin/pins/create">핀번호 생성</Link>
-                  </li>
-                  <li className="text-sm mt-[2px] text-gray-600">
-                    <Link href="/admin/pins/register">핀번호 등록</Link>
-                  </li>
-
-                  {/* <li className="text-sm mt-[2px] text-gray-600">핀번호 사용</li> */}
-                </ul>
-              </li>
-            </ul>
+    <AdminProtectedRoute>
+      <Loading />
+      <div className="container flex py-12 gap-6">
+        <div className="w-[260px] border border-gray-200 bg-white">
+          <div className="flex items-center justify-center py-4 text-white bg-black">
+            <h2 className="text-2xl">관리자 페이지</h2>
           </div>
-          <div className="flex-grow bg-white p-6">{children}</div>
+          <ul>
+            <li className="p-5 my-4">
+              <span className="flex items-center gap-1 text-xl font-bold">
+                상품 관리
+                <TbTicket />
+              </span>
+              <ul className="mt-2">
+                <li className="text-sm mt-[2px] text-gray-600">
+                  <Link href="/admin/products"> 상품 목록</Link>
+                </li>
+                <li className="text-sm mt-[2px] text-gray-600">
+                  <Link href="/admin/products/create">상품 등록</Link>
+                </li>
+              </ul>
+            </li>
+            <Separator />
+            <li className="p-5 my-4">
+              <span className="flex items-center gap-1 text-xl font-bold">
+                회원 관리
+                <TbUser />
+              </span>
+              <ul className="mt-2">
+                <li className="text-sm mt-[2px] text-gray-600">
+                  <Link href="/admin/user-categories">회원 구분</Link>
+                </li>
+                <li className="text-sm mt-[2px] text-gray-600">
+                  <Link href="/admin/users">회원 목록</Link>
+                </li>
+                <li className="text-sm mt-[2px] text-gray-600">
+                  파트너사 목록
+                </li>
+              </ul>
+            </li>
+            <Separator />
+            <li className="p-5 my-4">
+              <span className="flex text-lg font-bold">
+                핀번호 관리
+                <TbNumber123 />
+              </span>
+              <ul className="mt-2">
+                <li className="text-sm mt-[2px] text-gray-600">
+                  <Link href="/admin/pins">핀번호 목록</Link>
+                </li>
+                <li className="text-sm mt-[2px] text-gray-600">
+                  <Link href="/admin/pins/create">핀번호 생성</Link>
+                </li>
+                <li className="text-sm mt-[2px] text-gray-600">
+                  <Link href="/admin/pins/register">핀번호 등록</Link>
+                </li>
+
+                {/* <li className="text-sm mt-[2px] text-gray-600">핀번호 사용</li> */}
+              </ul>
+            </li>
+          </ul>
         </div>
-      </AdminProtectedRoute>
-    </AdminLoadingProvider>
+        <div className="flex-grow bg-white p-6">{children}</div>
+      </div>
+    </AdminProtectedRoute>
   );
 }
--- src/app/admin/loading.component.tsx
+++ src/app/admin/loading.component.tsx
@@ -1,8 +1,14 @@
-export default function AdminLoading({
-  isLoading = false,
-}: {
-  isLoading?: boolean;
-}) {
+'use client';
+
+import { useIsFetching } from '@tanstack/react-query';
+
+export default function Loading() {
+  const isFetching: boolean = !!useIsFetching();
+
+  if (!isFetching) {
+    return null;
+  }
+
   return (
     <div className="absolute w-full h-full grid place-items-center z-10">
       <svg className="animate-spin rounded-full border-4 border-gray-300 border-t-gray-900 h-12 w-12"></svg>
