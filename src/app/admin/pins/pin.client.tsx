4ee9387f3aae9511bb39acd44f9ee15868943e66
김태성 <qwaszx82@danbiedu.co.kr>
2024-08-31 16:08:28 +0900

    pagination추가

--- src/app/admin/pins/pin.client.tsx
+++ src/app/admin/pins/pin.client.tsx
@@ -4,6 +4,15 @@ import { useDeletePinMutation, usePinsListQuery } from '../queries';
 import { splitFourChar } from './pin.utils';
 import { Button } from '@/components/ui/button';
 import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+import {
   Table,
   TableBody,
   TableCaption,
@@ -46,62 +55,81 @@ export default function PinClient() {
   };
 
   return (
-    <Table>
-      {/* {isFetching && <TableCaption>조회 중입니다.</TableCaption>} */}
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[50px]">번호</TableHead>
-          <TableHead className="w-[200px]">핀 번호</TableHead>
-          <TableHead className="">연결 상품</TableHead>
-          <TableHead className="w-[110px] text-center">종료일</TableHead>
-          <TableHead className="w-[110px] text-center">생성일</TableHead>
-          <TableHead className="w-[70px] text-center">사용여부</TableHead>
-          <TableHead className="w-[110px] text-center">삭제</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {pinData.list.map((pin: Pin, idx: number) => (
-          <TableRow key={pin._id}>
-            <TableCell>{idx + 1}</TableCell>
-            <TableCell className="font-medium">
-              {splitFourChar(pin.number)}
-            </TableCell>
-            <TableCell
-              className="cursor-pointer"
-              onClick={() => handlePinListClick(pin.product?._id)}
-            >
-              {pin.product?.name}
-            </TableCell>
-            <TableCell className="text-right">
-              {pin.endDate && new Date(pin.endDate).toLocaleDateString()}
-            </TableCell>
-            <TableCell className="text-right">
-              {pin.createdAt && new Date(pin.createdAt).toLocaleDateString()}
-            </TableCell>
-            <TableCell className="text-center"></TableCell>
-            <TableCell className="text-center">
-              <Button
-                onClick={() =>
-                  handleDeletePin({
-                    id: pin._id ?? '',
-                    number: pin.number ?? '',
-                  })
-                }
+    <div>
+      <Table>
+        {/* {isFetching && <TableCaption>조회 중입니다.</TableCaption>} */}
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[50px]">번호</TableHead>
+            <TableHead className="w-[200px]">핀 번호</TableHead>
+            <TableHead className="">연결 상품</TableHead>
+            <TableHead className="w-[110px] text-center">종료일</TableHead>
+            <TableHead className="w-[110px] text-center">생성일</TableHead>
+            <TableHead className="w-[70px] text-center">사용여부</TableHead>
+            <TableHead className="w-[110px] text-center">삭제</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {pinData.list.map((pin: Pin, idx: number) => (
+            <TableRow key={pin._id}>
+              <TableCell>{idx + 1}</TableCell>
+              <TableCell className="font-medium">
+                {splitFourChar(pin.number)}
+              </TableCell>
+              <TableCell
+                className="cursor-pointer"
+                onClick={() => handlePinListClick(pin.product?._id)}
               >
-                삭제
-              </Button>
+                {pin.product?.name}
+              </TableCell>
+              <TableCell className="text-right">
+                {pin.endDate && new Date(pin.endDate).toLocaleDateString()}
+              </TableCell>
+              <TableCell className="text-right">
+                {pin.createdAt && new Date(pin.createdAt).toLocaleDateString()}
+              </TableCell>
+              <TableCell className="text-center"></TableCell>
+              <TableCell className="text-center">
+                <Button
+                  onClick={() =>
+                    handleDeletePin({
+                      id: pin._id ?? '',
+                      number: pin.number ?? '',
+                    })
+                  }
+                >
+                  삭제
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={6}>총 상품</TableCell>
+            <TableCell className="text-right">
+              {addComma(pinData.list.length)} 개
             </TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={6}>총 상품</TableCell>
-          <TableCell className="text-right">
-            {addComma(pinData.list.length)} 개
-          </TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
+        </TableFooter>
+      </Table>
+      {/* 
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination> */}
+    </div>
   );
 }
