ab604acd377c309216bb45264e171699c66cf305
KimTaeSung <qwasszx82@gmail.com>
2024-10-10 08:17:59 +0900

    개발 진행중

--- src/app/(home)/home.client.tsx
+++ src/app/(home)/home.client.tsx
@@ -38,10 +38,10 @@ export default function HomeClient() {
                     marginTop: 'calc((3rem)* -1)',
                   }}
                 >
-                  <div className="absolute">
+                  {/* <div className="absolute">
                     <div className="animate-fade">재고량 : </div>
                     <div className="animate-fade">사용량 : </div>
-                  </div>
+                  </div> */}
                   {images[0] && (
                     <Image
                       alt="상품 이미지"
