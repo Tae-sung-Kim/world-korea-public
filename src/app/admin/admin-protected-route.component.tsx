7abf98c3ff876ad9380b37fcc97782b97d13196e
KimTaeSung <qwasszx82@gmail.com>
2024-11-11 16:03:39 +0900

    권한에 맞게 url 처리

--- src/app/admin/admin-protected-route.component.tsx
+++ src/app/admin/admin-protected-route.component.tsx
@@ -1,21 +1,51 @@
 'use client';
 
 import { useAuthContext } from '@/contexts/auth.context';
-import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
+import { USER_ROLE } from '@/definitions';
+import { redirect, usePathname } from 'next/navigation';
+import { useEffect, useMemo } from 'react';
+
+type UserRole = (typeof USER_ROLE)[keyof typeof USER_ROLE];
+
+const restrictedPaths: Record<UserRole, string[]> = {
+  partner: ['/partner/'],
+  user: ['/partner/', '/admin/'],
+  admin: [],
+};
 
 export default function AdminProtectedRoute({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { isLoggedIn } = useAuthContext();
+  const { isLoggedIn, user } = useAuthContext();
+  const pathName = usePathname();
+
+  const userRole = useMemo(() => user?.role as UserRole, [user?.role]);
+
+  const { isUrlValid } = useMemo(() => {
+    if (!userRole || !pathName) {
+      return { isUrlValid: false };
+    }
+
+    const restrictedPathsForRole = restrictedPaths[userRole] ?? [];
+
+    return {
+      isUrlValid: !restrictedPathsForRole.some((restrictedPath) =>
+        pathName.includes(restrictedPath)
+      ),
+    };
+  }, [userRole, pathName]);
 
   useEffect(() => {
-    if (isLoggedIn === false) {
+    if (isLoggedIn === false || (userRole === 'user' && !isUrlValid)) {
+      // 일반 회원은 admin, partner 경로 접근 제한
       redirect('/');
+    } else if (userRole === 'partner' && !isUrlValid) {
+      // 파트너는 파트너 경로 외 접근 제한
+      redirect('/partner/orders');
     }
-  }, [isLoggedIn]);
+  }, [isLoggedIn, userRole, isUrlValid]);
 
-  return children;
+  return isLoggedIn && isUrlValid ? children : null;
 }
