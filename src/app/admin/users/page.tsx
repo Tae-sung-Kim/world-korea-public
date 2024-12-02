25b77bc97457c68dbc5ca181c78243ae1cf3f71b
김태성 <qwasszx82@gmail.com>
2024-12-03 00:22:00 +0900

    user 영역 처리

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,23 +1,5 @@
 import UsersClient from './users.client';
 
-// import userService from '@/services/userService';
-// import {
-//   HydrationBoundary,
-//   QueryClient,
-//   dehydrate,
-// } from '@tanstack/react-query';
-
 export default async function UsersPage() {
-  // const queryClient = new QueryClient();
-
-  // await queryClient.prefetchQuery({
-  //   queryKey: ['users'],
-  //   queryFn: userService.getUserList,
-  // });
-
-  return (
-    // <HydrationBoundary state={dehydrate(queryClient)}>
-    <UsersClient />
-    // </HydrationBoundary>
-  );
+  return <UsersClient />;
 }
--- src/app/admin/users/users.client.tsx
+++ src/app/admin/users/users.client.tsx
@@ -1,83 +1,25 @@
 'use client';
 
 import { useUserListQuery } from '../queries';
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
+import UserCard from './components/user-card.component';
 import { User } from '@/definitions';
 import { useRouter } from 'next/navigation';
 
 export default function UsersClient() {
   const userList = useUserListQuery();
-
   const router = useRouter();
 
-  const handleTrClick = (userData: User) => () => {
-    router.push(`/admin/users/${userData._id}`);
+  const handleUserClick = (user: User) => {
+    router.push(`/admin/users/${user._id}`);
   };
 
   return (
-    <div className="container">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>분류</TableHead>
-            <TableHead>아이디</TableHead>
-            <TableHead>이름</TableHead>
-            <TableHead>업체No</TableHead>
-            <TableHead>업체명</TableHead>
-            <TableHead>파트너여부</TableHead>
-            <TableHead>이메일</TableHead>
-            <TableHead>연락처</TableHead>
-            <TableHead>등록일</TableHead>
-            <TableHead>승인</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {userList.map((user) => {
-            const {
-              _id,
-              userCategory,
-              loginId,
-              name,
-              companyNo,
-              companyName,
-              email,
-              contactNumber,
-              createdAt,
-              isApproved,
-              isPartner,
-            } = user;
-
-            return (
-              <TableRow
-                className="cursor-pointer"
-                key={_id}
-                onClick={handleTrClick(user)}
-              >
-                <TableCell>{userCategory?.name}</TableCell>
-                <TableCell>{loginId}</TableCell>
-                <TableCell>{name}</TableCell>
-                <TableCell>{companyNo}</TableCell>
-                <TableCell>{companyName}</TableCell>
-                <TableCell>{isPartner ? 'Y' : 'N'}</TableCell>
-                <TableCell>{email}</TableCell>
-                <TableCell>{contactNumber}</TableCell>
-                <TableCell>
-                  {createdAt && new Date(createdAt).toLocaleDateString()}
-                </TableCell>
-                <TableCell>{isApproved ? '승인' : '미승인'}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
+    <div className="container mx-auto py-8 px-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {userList.map((user) => (
+          <UserCard key={user._id} user={user} onClick={handleUserClick} />
+        ))}
+      </div>
     </div>
   );
 }
