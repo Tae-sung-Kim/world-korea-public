0b33f0859e48140a189d84204c6ce241b1c94e04
남영곤 <nyg@danbiedu.co.kr>
2024-12-22 18:51:58 +0900

    내 구매목록 반환 API 추가, /api/orders/me

--- src/app/api/models/order.model.ts
+++ src/app/api/models/order.model.ts
@@ -58,6 +58,9 @@ interface OrderSchemaModel extends Model<OrderDB, {}, OrderMethods> {
   getOrderList(
     paginationParams: PaginationParams
   ): PaginationResponse<Promise<Order[]>>;
+  getOrderMyList(
+    paginationParams: PaginationParams, userId: string
+  ): PaginationResponse<Promise<Order[]>>;
   checkShortIdExists(shortId: string): Promise<boolean>; // shortUrl 이 이미 있는지 여부 반환
   getOrderByShortId(shortId: string): Promise<OrderDocument | null>;
   getSaleProductIdByShortId(shortId: string): Promise<string | null>;
@@ -129,6 +132,73 @@ schema.static(
     // 데이터 가져오기
     let list = (
       await this.find(filter)
+        .sort(sort)
+        .skip(skip)
+        .limit(pageSize)
+        .populate({
+          path: 'user',
+          select: '_id name companyName',
+        })
+        .populate({
+          path: 'saleProduct',
+          select: '_id name',
+        })
+    ).map((d) => d.toObject());
+
+    // 전체 페이지 수 계산
+    const totalPages = Math.ceil(totalItems / pageSize);
+
+    // 페이지네이션 관련 정보 계산
+    const hasPreviousPage = pageNumber > 1;
+    const hasNextPage = pageNumber < totalPages;
+    const previousPage = hasPreviousPage ? pageNumber - 1 : null;
+    const nextPage = hasNextPage ? pageNumber + 1 : null;
+
+    return {
+      list,
+      pageNumber,
+      pageSize,
+      totalItems,
+      totalPages,
+      hasPreviousPage,
+      hasNextPage,
+      previousPage,
+      nextPage,
+      startIndex: skip,
+      endIndex: totalItems - 1,
+    };
+  }
+);
+
+schema.static(
+  'getOrderMyList',
+  async function getOrderMyList({
+    pageNumber = PAGE_NUMBER_DEFAULT,
+    pageSize = PAGE_SIZE_DEFAULT,
+    filter: filterQuery = null,
+  } = {}, userId) {
+    const skip = (pageNumber - 1) * pageSize;
+    const filter: Record<string, any> = {
+      // accessLevel: { $lte: level },
+    };
+    const sort = { createdAt: -1 as SortOrder }; // 최신순 정렬
+
+    if (filterQuery) {
+      Object.keys(filterQuery).forEach((key) => {
+        const value = filterQuery[key];
+        filter[key] = { $regex: value, $options: 'i' }; // 정규식 검색 적용
+      });
+    }
+
+    // 총 개수 가져오기
+    const totalItems = await this.countDocuments(filter);
+
+    // 데이터 가져오기
+    let list = (
+      await this.find({
+          ...filter,
+          user: userId,
+        })
         .sort(sort)
         .skip(skip)
         .limit(pageSize)
---
+++ src/app/api/orders/me/route.ts
@@ -0,0 +1,34 @@
+import OrderModel from '../../models/order.model';
+import { getQueryParams } from '../../utils/api.utils';
+import { getCurrentUser } from '../../utils/auth.util';
+import { createResponse } from '../../utils/http.util';
+import connectMongo from '@/app/api/libs/database';
+import {
+  HTTP_STATUS,
+} from '@/definitions';
+import { NextRequest, NextResponse } from 'next/server';
+
+/**
+ * 주문 목록 반환
+ */
+export async function GET(req: NextRequest) {
+  try {
+    await connectMongo();
+
+    const userData = await getCurrentUser();
+    if (!userData) {
+      return createResponse(HTTP_STATUS.UNAUTHORIZED);
+    }
+
+    const { pageNumber, pageSize, filter } = getQueryParams(req);
+    const paginationResponse = await OrderModel.getOrderMyList({
+      pageNumber,
+      pageSize,
+      filter
+    }, userData._id);
+
+    return NextResponse.json(paginationResponse);
+  } catch (error) {
+    return createResponse(HTTP_STATUS.INTERNAL_SERVER_ERROR);
+  }
+}
